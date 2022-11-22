a3d9363bc558cbef5b11ae1c265736afd3409041
Emik <emik.lakunishok@gmail.com>
2022-11-22 23:34:10 +0100

    Change Clamp overloads to generic in .NET 6+

--- Source/Clamped.cs
+++ Source/Clamped.cs
@@ -6,6 +6,7 @@ namespace Emik.Morsels;
 /// <summary>Extension methods to clamp numbers.</summary>
 static class Clamped
 {
+#if !NET6_0_OR_GREATER
     /// <summary>Clamps a value such that it is no smaller or larger than the defined amount.</summary>
     /// <param name="number">The number to clip.</param>
     /// <param name="min">If specified, the smallest number to return.</param>
@@ -29,4 +30,32 @@ static class Clamped
         (max ?? number) is var big &&
         number <= small ? small :
         number >= big ? big : number;
+#else
+    /// <summary>Clamps a value such that it is no smaller or larger than the defined amount.</summary>
+    /// <param name="number">The number to clip.</param>
+    /// <param name="min">If specified, the smallest number to return.</param>
+    /// <param name="max">If specified, the greatest number to return.</param>
+    /// <returns>
+    /// The parameter <paramref name="min"/> if <paramref name="number"/> is smaller than <paramref name="min"/>,
+    /// otherwise, the parameter <paramref name="max"/> if <paramref name="number"/> is greater than
+    /// <paramref name="max"/>, otherwise the parameter <paramref name="number"/>.
+    /// </returns>
+    [Pure]
+    internal static T Clamp<T>(this T number, T? min = null, T? max = null)
+        where T : class, IComparisonOperators<T, T, bool> =>
+        (min ?? number) is var small &&
+        (max ?? number) is var big &&
+        number <= small ? small :
+        number >= big ? big : number;
+
+    /// <inheritdoc cref="Clamp{T}(T, T?, T?)"/>
+    [Pure]
+    internal static T Clamp<T>(this T number, T? min = null, T? max = null)
+        where T : struct, IComparisonOperators<T, T, bool> =>
+        (min ?? number) is var small &&
+        (max ?? number) is var big &&
+        number <= small ? small :
+        number >= big ? big : number;
+
+#endif
 }
