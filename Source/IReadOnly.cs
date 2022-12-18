a3e038a320ef79dc426d42acf04cf6b0cc1a8dc5
Emik <emik.lakunishok@gmail.com>
2022-12-18 21:51:16 +0100

    Add IReadOnlyDictionary

--- Source/IReadOnly.cs
+++ Source/IReadOnly.cs
@@ -13,6 +13,43 @@ partial interface IReadOnlyCollection<out T> : IEnumerable<T>
     int Count { get; }
 }
 
+/// <summary>Represents a generic read-only collection of key/value pairs.</summary>
+/// <typeparam name="TKey">The type of keys in the read-only dictionary.</typeparam>
+/// <typeparam name="TValue">The type of values in the read-only dictionary.</typeparam>
+partial interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>
+{
+    /// <summary>Gets an enumerable collection that contains the keys in the read-only dictionary.</summary>
+    IEnumerable<TKey> Keys { get; }
+
+    /// <summary>Gets an enumerable collection that contains the values in the read-only dictionary.</summary>
+    IEnumerable<TValue> Values { get; }
+
+    /// <summary>Gets the element that has the specified key in the read-only dictionary.</summary>
+    /// <param name="key">The key to locate.</param>
+    TValue this[TKey key] { get; }
+
+    /// <summary>Determines whether the read-only dictionary contains an element that has the specified key.</summary>
+    /// <param name="key">The key to locate.</param>
+    /// <returns>
+    /// <see langword="true"/> if the read-only dictionary contains an element that has the specified key;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    bool ContainsKey(TKey key);
+
+    /// <summary>Gets the value that is associated with the specified key.</summary>
+    /// <param name="key">The key to locate.</param>
+    /// <param name="value">
+    /// When this method returns, the value associated with the specified key, if the key is found;
+    /// otherwise, the default value for the type of the <paramref name="value"/> parameter.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the object that implements the <see cref="IReadOnlyDictionary{TKey,TValue}"/>
+    /// interface contains an element that has the specified key; otherwise, <see langword="false"/>.
+    /// </returns>
+    bool TryGetValue(TKey key, out TValue value);
+}
+
 /// <summary>Represents a read-only collection of elements that can be accessed by index.</summary>
 /// <typeparam name="T">The type of elements in the read-only list.</typeparam>
 partial interface IReadOnlyList<out T> : IReadOnlyCollection<T>
