5d615adb34ab89cb9b26763d8556c5737e2a7d9e
Emik <emik.lakunishok@gmail.com>
2022-12-29 20:03:52 +0100

    Add XML documentation

--- Source/TargetFrameworkAttribute.fs
+++ Source/TargetFrameworkAttribute.fs
@@ -3,15 +3,18 @@ namespace System.Runtime.Versioning
 
 open System
 
+/// Identifies the version of .NET that a particular assembly was compiled against.
 [<AttributeUsage(AttributeTargets.Assembly); Sealed>]
 type TargetFrameworkAttribute (frameworkName : string) =
     inherit Attribute ()
     let mutable _frameworkDisplayName = null
     let _frameworkName = frameworkName
-    member this.FrameworkName = _frameworkName
 
+    /// Gets the display name of the .NET version against which an assembly was built.
     member this.FrameworkDisplayName
         with get () = _frameworkName
         and set (value : string) = _frameworkDisplayName <- value
 
+    /// Gets the name of the .NET version against which a particular assembly was compiled.
+    member this.FrameworkName = _frameworkName
 #endif
