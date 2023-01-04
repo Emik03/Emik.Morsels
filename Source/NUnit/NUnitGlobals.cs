cb183735ddf2a0cba6489108be2ac07158209292
Emik <emik.lakunishok@gmail.com>
2023-01-04 18:09:51 +0100

    Add globals for NUnit

---
+++ Source/NUnit/NUnitGlobals.cs
@@ -0,0 +1,31 @@
+#region Emik.MPL
+
+// <copyright file="NUnitGlobals.cs" company="Emik">
+// Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+#endregion
+
+#region
+
+// ReSharper disable RedundantUsingDirective.Global
+global using NUnit;
+global using NUnit.Compatibility;
+global using NUnit.Framework;
+global using NUnit.Framework.Api;
+global using NUnit.Framework.Constraints;
+global using NUnit.Framework.Constraints.Comparers;
+global using NUnit.Framework.Interfaces;
+global using NUnit.Framework.Internal;
+global using NUnit.Framework.Internal.Abstractions;
+global using NUnit.Framework.Internal.Builders;
+global using NUnit.Framework.Internal.Commands;
+global using NUnit.Framework.Internal.Execution;
+global using NUnit.Framework.Internal.Extensions;
+global using NUnit.Framework.Internal.Filters;
+global using static NUnit.Framework.Assert;
+global using static NUnit.Framework.Has;
+global using static NUnit.Framework.Is;
+global using static NUnit.Framework.StringAssert;
+
+#endregion
