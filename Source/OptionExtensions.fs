23735eb9b73d33bd41738a96cd0656e9c93cc8ca
Emik <emik.lakunishok@gmail.com>
2023-01-05 11:30:23 +0100

    Add option extensions

---
+++ Source/OptionExtensions.fs
@@ -0,0 +1,16 @@
+/// Extension methods for option types.
+module Emik.Morsels.OptionExtensions
+
+type Microsoft.FSharp.Core.Option<'a> with
+
+    /// Gets the value, or the fallback.
+    member this.orGet f =
+        match this with
+        | Some x -> x
+        | None -> f
+
+    /// Gets the value, or invokes the callback.
+    member this.orGet f =
+        match this with
+        | Some x -> x
+        | None -> f ()
