66aeeeef881c777d83ab8bf5031d43c9eab744ba
Emik <emik.lakunishok@gmail.com>
2023-01-05 11:30:30 +0100

    Improve randomizer

--- Source/Randomizer.fs
+++ Source/Randomizer.fs
@@ -1,13 +1,15 @@
 /// Defines operators for functions.
 module Emik.Morsels.Randomizer
 
+open System
+
 /// Shuffles a sequence based on a function.
-let shuffle next xs = xs |> Seq.sortBy (fun _ -> next ())
+let shuffle fn xs = xs |> Seq.sortBy (fun _ -> fn Int32.MinValue Int32.MaxValue)
 
 /// Picks a random element from a sequence based on a function.
-let pick next xs = xs |> shuffle next |> Seq.nth 0
+let pick fn xs = xs |> shuffle fn |> Seq.nth 0
 
 /// Attempts to pick a random element from a sequence based on a function.
 /// Returns None if the sequence is empty.
-let tryPick next xs =
-    if xs |> Seq.isEmpty then None else Some (xs |> shuffle next |> Seq.nth 0)
+let tryPick fn xs =
+    if xs |> Seq.isEmpty then None else Some (xs |> shuffle fn |> Seq.nth 0)
