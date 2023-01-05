9197a1688bb514de3fcc07aaf1aea95f502584df
Emik <emik.lakunishok@gmail.com>
2023-01-05 10:29:51 +0100

    Add tryPick

--- Source/Randomizer.fs
+++ Source/Randomizer.fs
@@ -6,3 +6,8 @@ let shuffle next xs = xs |> Seq.sortBy (fun _ -> next ())
 
 /// Picks a random element from a sequence based on a function.
 let pick next xs = xs |> shuffle next |> Seq.nth 0
+
+/// Attempts to pick a random element from a sequence based on a function.
+/// Returns None if the sequence is empty.
+let tryPick next xs =
+    if xs |> Seq.isEmpty then None else Some (xs |> shuffle next |> Seq.nth 0)
