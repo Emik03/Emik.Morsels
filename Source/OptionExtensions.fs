0af682e94cd7d284f4c1e5692ff5017e0ff0f6e6
Emik <emik.lakunishok@gmail.com>
2023-01-08 23:12:03 +0100

    Change extension method name

--- Source/OptionExtensions.fs
+++ Source/OptionExtensions.fs
@@ -4,13 +4,13 @@ module internal Emik.Morsels.OptionExtensions
 type Microsoft.FSharp.Core.Option<'a> with
 
     /// Gets the value, or the fallback.
-    member this.orGet f =
+    member this.getOr f =
         match this with
         | Some x -> x
         | None -> f
 
     /// Gets the value, or invokes the callback.
-    member this.orGet f =
+    member this.getOr f =
         match this with
         | Some x -> x
         | None -> f ()
