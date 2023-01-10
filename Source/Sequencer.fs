2c184342e0577991d973e42c2704fc6d5b534f35
Emik <emik.lakunishok@gmail.com>
2023-01-10 15:49:16 +0100

    Add function that removes an element from a list

--- Source/Sequencer.fs
+++ Source/Sequencer.fs
@@ -6,10 +6,7 @@ let contains i s = s |> Seq.exists ((=) i)
 
 /// Determines whether the coordinates are in range for a 2D array.
 let inBounds y x a =
-    Array2D.base1 a <= y
-    && Array2D.base2 a <= x
-    && y < Array2D.length1 a
-    && x < Array2D.length2 a
+    Array2D.base1 a <= y && Array2D.base2 a <= x && y < Array2D.length1 a && x < Array2D.length2 a
 
 /// Determines whether the coordinates are in range for a 3D array.
 let inBounds3 z y x a =
@@ -34,17 +31,14 @@ let inBounds4 w z y x a =
 /// Gets the jagged array from the 2D array.
 let toJagged (a : _[,]) =
     [| for y in a.GetLowerBound 0 .. a.GetLength 0 - 1 do
-           yield
-               [| for x in a.GetLowerBound 1 .. a.GetLength 1 - 1 -> a[y, x] |] |]
+           yield [| for x in a.GetLowerBound 1 .. a.GetLength 1 - 1 -> a[y, x] |] |]
 
 /// Gets the jagged array from the 3D array.
 let toJagged3 (a : _[,,]) =
     [| for z in a.GetLowerBound 0 .. a.GetLength 0 - 1 do
            yield
                [| for y in a.GetLowerBound 1 .. a.GetLength 1 - 1 do
-                      yield
-                          [| for x in a.GetLowerBound 2 .. a.GetLength 2 - 1 ->
-                                 a[z, y, x] |] |] |]
+                      yield [| for x in a.GetLowerBound 2 .. a.GetLength 2 - 1 -> a[z, y, x] |] |] |]
 
 /// Gets the jagged array from the 4D array.
 let toJagged4 (a : _[,,,]) =
@@ -54,9 +48,7 @@ let toJagged4 (a : _[,,,]) =
                       yield
                           [| for y in a.GetLowerBound 2 .. a.GetLength 2 - 1 do
                                  yield
-                                     [| for x in
-                                            a.GetLowerBound 3 .. a.GetLength 3
-                                                                 - 1 ->
+                                     [| for x in a.GetLowerBound 3 .. a.GetLength 3 - 1 ->
                                             a[w, z, y, x] |] |] |] |]
 
 /// Gets the sequence from the 2D array.
@@ -93,5 +85,14 @@ let tryGet y x a = if a |> inBounds y x then Some (a[y, x]) else None
 let tryGet3 z y x a = if a |> inBounds3 z y x then Some (a[z, y, x]) else None
 
 /// Returns the corresponding index if in range for a 4D array, else None.
-let tryGet4 w z y x a =
-    if a |> inBounds4 w z y x then Some (a[w, z, y, x]) else None
+let tryGet4 w z y x a = if a |> inBounds4 w z y x then Some (a[w, z, y, x]) else None
+
+/// Removes the item from the list. Only the first occurence of the item will be removed.
+let remove n list =
+    let rec go n list acc =
+        match list with
+        | h :: tl when h = n -> acc @ tl
+        | h :: tl -> h :: acc |> go n tl
+        | [] -> acc
+
+    go n list []
