176010ee22b55b579e30d0a4d5688dec3e1863da
Emik <emik.lakunishok@gmail.com>
2023-01-12 12:10:06 +0100

    Add C# skeleton project

---
+++ CSharpProject.skel/Source/Program.cs
@@ -0,0 +1,9 @@
+#region Emik.MPL
+
+// <copyright file="Program.cs" company="Emik">
+// Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+#endregion
+
+Console.WriteLine("Hello, World! ma o, toki!");
