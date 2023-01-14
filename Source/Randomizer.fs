a81edb1e6a1512d9ff97851a978aee54eb95019c
Emik <emik.lakunishok@gmail.com>
2023-01-14 01:19:02 +0100

    Use Seq.item when available

--- Source/Randomizer.fs
+++ Source/Randomizer.fs
@@ -7,15 +7,18 @@ open System
 let shuffle fn xs = xs |> Seq.sortBy (fun _ -> fn Int32.MinValue Int32.MaxValue)
 
 /// Picks the random element from the sequence based on the function.
+#if NETFRAMEWORK && !NET40_OR_GREATER
 let pickRandom fn xs = xs |> shuffle fn |> Seq.nth 0
+#else
+let pickRandom fn xs = xs |> shuffle fn |> Seq.item 0
+#endif
 
 /// Picks the random index from the sequence based on the function.
 let pickIndex fn (xs : _[]) = fn (xs.GetLowerBound 0) xs.Length
 
 /// Picks the random index from the sequence based on the function.
 let pickIndex2 fn (xs : _[,]) =
-    (fn (xs.GetLowerBound 0) (xs.GetLength 0),
-     fn (xs.GetLowerBound 1) (xs.GetLength 1))
+    (fn (xs.GetLowerBound 0) (xs.GetLength 0), fn (xs.GetLowerBound 1) (xs.GetLength 1))
 
 /// Picks the random index from the sequence based on the function.
 let pickIndex3 fn (xs : _[,,]) =
@@ -35,5 +38,4 @@ let toBoolRng fn = (fun _ -> fn 0 2 = 0)
 
 /// Attempts to pick a random element from a sequence based on a function.
 /// Returns None if the sequence is empty.
-let tryPickRandom fn xs =
-    if xs |> Seq.isEmpty then None else Some (xs |> shuffle fn |> Seq.nth 0)
+let tryPickRandom fn xs = if xs |> Seq.isEmpty then None else Some (xs |> shuffle fn |> Seq.nth 0)
