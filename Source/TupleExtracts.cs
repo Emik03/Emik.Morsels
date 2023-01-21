075be9c56a309f50426fb210e6914553f08c1e42
Emik <emik.lakunishok@gmail.com>
2023-01-21 20:19:31 +0100

    Unextend the methods

--- Source/TupleExtracts.cs
+++ Source/TupleExtracts.cs
@@ -13,12 +13,12 @@ static partial class TupleExtracts
     /// <typeparam name="T2">The second type of the tuple.</typeparam>
     /// <param name="tuple">The tuple to get the value from.</param>
     /// <returns>The field <see cref="ValueTuple{T1, T2}.Item1"/> from the parameter <paramref name="tuple"/>.</returns>
-    public static T1 First<T1, T2>(this (T1, T2) tuple) => tuple.Item1;
+    public static T1 First<T1, T2>((T1, T2) tuple) => tuple.Item1;
 
     /// <summary>Gets the second item of the tuple.</summary>
     /// <typeparam name="T1">The first type of the tuple.</typeparam>
     /// <typeparam name="T2">The second type of the tuple.</typeparam>
     /// <param name="tuple">The tuple to get the value from.</param>
     /// <returns>The field <see cref="ValueTuple{T1, T2}.Item2"/> from the parameter <paramref name="tuple"/>.</returns>
-    public static T2 Second<T1, T2>(this (T1, T2) tuple) => tuple.Item2;
+    public static T2 Second<T1, T2>((T1, T2) tuple) => tuple.Item2;
 }
