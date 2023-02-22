4579b4711fdb1ef2b060ed7f2d5ce30539a404b1
Emik <emik.lakunishok@gmail.com>
2023-02-22 13:23:54 +0100

    Add lint comment

--- Compile/Source/References/Emik/Linq/Pancake.cs
+++ Compile/Source/References/Emik/Linq/Pancake.cs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
+
+// ReSharper disable once CheckNamespace
 namespace Emik.Morsels;
 
 /// <summary>Provides methods to flatten <see cref="IEnumerable{T}"/> instances.</summary>
