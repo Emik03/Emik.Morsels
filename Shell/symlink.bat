6195e9ae0b78fb02e84dd4f86dc75f12d79470b1
Emik <emik.lakunishok@gmail.com>
2023-03-16 21:32:35 +0100

    Fix relative paths

--- Shell/symlink.bat
+++ Shell/symlink.bat
@@ -1,2 +1,1 @@
-mklink global.json %0 && mklink stylecop.json %0 && mklink Directory.Build.props %0 && mklink Directory.Build.targets %0 && mklink Directory.Build.local.props %0
-
+mklink Emik.Morsels/Content/Properties/global.json %0 && mklink Emik.Morsels/Content/Properties/stylecop.json %0 && mklink Emik.Morsels/Content/Properties/Directory.Build.props %0 && mklink Emik.Morsels/Content/Properties/Directory.Build.targets %0 && mklink Emik.Morsels/Content/Properties/Directory.Build.local.props %0
