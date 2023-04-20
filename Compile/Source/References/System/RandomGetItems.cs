2f96344429f02893f236d8eeb2179a66c592454a
Emik <emik.lakunishok@gmail.com>
2023-04-20 09:56:21 +0200

    Add Random polyfills

---
+++ Compile/Source/References/System/RandomGetItems.cs
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MPL-2.0
+#if !NET8_0_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace System;
+
+/// <summary>The backport of GetItems methods for <see cref="Random"/>.</summary>
+static partial class RandomGetItems
+{
+    /// <summary>
+    /// Fills the elements of a specified span with items chosen at random from the provided set of choices.
+    /// </summary>
+    /// <remarks><para>
+    /// The method uses <see cref="Random.Next(int)"/> to select items randomly from <paramref name="choices"/>
+    /// by index and populate <paramref name="destination"/>.
+    /// </para></remarks>
+    /// <typeparam name="T">The type of span.</typeparam>
+    /// <param name="that">The instance of <see cref="Random"/>.</param>
+    /// <param name="choices">The items to use to populate the span.</param>
+    /// <param name="destination">The span to be filled with items.</param>
+    /// <exception cref="InvalidOperationException"><paramref name="choices"/> is empty.</exception>
+    public static void GetItems<T>(this Random that, ReadOnlySpan<T> choices, Span<T> destination)
+    {
+        if (choices.IsEmpty)
+            throw CannotBeEmpty;
+
+        for (var i = 0; i < destination.Length; i++)
+            destination[i] = choices[that.Next(choices.Length)];
+    }
+
+    /// <summary>Creates an array populated with items chosen at random from the provided set of choices.</summary>
+    /// <remarks><para>
+    /// The method uses <see cref="Random.Next(int)"/> to select items randomly from <paramref name="choices"/>
+    /// by index. This is used to populate a newly-created array.
+    /// </para></remarks>
+    /// <typeparam name="T">The type of array.</typeparam>
+    /// <param name="that">The instance of <see cref="Random"/>.</param>
+    /// <param name="choices">The items to use to populate the array.</param>
+    /// <param name="length">The length of array to return.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="length"/> is not zero or a positive number.
+    /// </exception>
+    /// <exception cref="InvalidOperationException"><paramref name="choices"/> is empty.</exception>
+    /// <returns>An array populated with random items.</returns>
+    public static T[] GetItems<T>(this Random that, T[] choices, [NonNegativeValue] int length) =>
+        GetItems(that, new ReadOnlySpan<T>(choices), length);
+
+    /// <summary>Creates an array populated with items chosen at random from the provided set of choices.</summary>
+    /// <remarks><para>
+    /// The method uses <see cref="Random.Next(int)"/> to select items randomly from <paramref name="choices"/>
+    /// by index. This is used to populate a newly-created array.
+    /// </para></remarks>
+    /// <typeparam name="T">The type of array.</typeparam>
+    /// <param name="that">The instance of <see cref="Random"/>.</param>
+    /// <param name="choices">The items to use to populate the array.</param>
+    /// <param name="length">The length of array to return.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="length"/> is not zero or a positive number.
+    /// </exception>
+    /// <exception cref="InvalidOperationException"><paramref name="choices"/> is empty.</exception>
+    /// <returns>An array populated with random items.</returns>
+    public static T[] GetItems<T>(this Random that, ReadOnlySpan<T> choices, [NonNegativeValue] int length)
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "Cannot be negative");
+
+        var items = new T[length];
+#if !(NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) || NO_SYSTEM_MEMORY
+        fixed (var t = items)
+            GetItems(that, choices, new Span<T>(t, items.Length));
+#else
+        GetItems(that, choices, items.AsSpan());
+#endif
+        return items;
+    }
+}
+#endif
---
+++ Compile/Source/References/System/RandomShuffle.cs
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MPL-2.0
+#if !NET8_0_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace System;
+
+/// <summary>The backport of Shuffle methods for <see cref="Random"/>.</summary>
+static partial class RandomShuffle
+{
+    /// <summary>Performs an in-place shuffle of an array.</summary>
+    /// <remarks><para>
+    /// This method uses <see cref="Random.Next(int, int)" /> to choose values for shuffling.
+    /// This method is an O(n) operation.
+    /// </para></remarks>
+    /// <typeparam name="T">The type of array.</typeparam>
+    /// <param name="that">The instance of <see cref="Random"/>.</param>
+    /// <param name="values">The array to shuffle.</param>
+    public static void Shuffle<T>(this Random that, T[] values)
+    {
+#if !(NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) || NO_SYSTEM_MEMORY
+        fixed (var t = values)
+            that.Shuffle(new(t, values.Length));
+#else
+        Shuffle(that, values.AsSpan());
+#endif
+    }
+
+    /// <summary>Performs an in-place shuffle of a span.</summary>
+    /// <typeparam name="T">The type of span.</typeparam>
+    /// <param name="that">The instance of <see cref="Random"/>.</param>
+    /// <param name="values">The span to shuffle.</param>
+    /// <remarks><para>
+    /// This method uses <see cref="Random.Next(int, int)" /> to choose values for shuffling.
+    /// This method is an O(n) operation.
+    /// </para></remarks>
+    public static void Shuffle<T>(this Random that, Span<T> values)
+    {
+        var n = values.Length;
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            var j = that.Next(i, n);
+
+            if (j == i)
+                continue;
+
+            // ReSharper disable once SwapViaDeconstruction
+#pragma warning disable IDE0180
+            var temp = values[i];
+            values[i] = values[j];
+            values[j] = temp;
+#pragma warning restore IDE0180
+        }
+    }
+}
+#endif
