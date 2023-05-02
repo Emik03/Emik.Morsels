3dfe7d5d03f5f304c0270fb6b89e8310a976c738
Emik <emik.lakunishok@gmail.com>
2023-05-02 20:13:13 +0200

    Bump shell references

--- Shell/csrepl.sh
+++ Shell/csrepl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-csharprepl -r "nuget:JetBrains.Annotations, 2022.3.1" "nuget:Emik.Results, 1.0.5" "nuget:Emik.Unions, 1.0.4" "$(dirname "$(dirname "$(realpath "$0")")")/Experimental/REPL.csx"
+csharprepl -r "nuget: JetBrains.Annotations, 2023.2.0-eap2" "nuget: Emik.Results, 1.0.5" "nuget: Emik.Unions, 1.0.4" "$(dirname "$(dirname "$(realpath "$0")")")/Experimental/REPL.csx"
 
