Emik03/Emik.Morsels

commit a6c6eebea85827ab07fc8e8429b3f7a6ad3f28ed

Re-support .NET Standard 1.0, which I accidentally broke

Compile/Source/References/Emik/Text/EnumStrings.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-#if !NETFRAMEWORK || NET40_OR_GREATER
+#if NET35_OR_GREATER || NETSTANDARD2_0_OR_GREATER || NETCOREAPP2_0_OR_GREATER
 // ReSharper disable once CheckNamespace RedundantNameQualifier
 namespace Emik.Morsels;
 

Compile/Source/References/Emik/Linq/Pancake.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-
+#if !NET20 && !NET30
 // ReSharper disable once CheckNamespace
 namespace Emik.Morsels;
 
@@ -33,3 +33,4 @@ static partial class Pancake
            .SelectMany(Enumerable.AsEnumerable)
            .SelectMany(Enumerable.AsEnumerable);
 }
+#endif

Compile/Source/References/Emik/Text/CharacterInvariance.cs
@@ -153,7 +153,9 @@ static partial class CharacterInvariance
         return "";
     }
 
+#if NETFRAMEWORK || NETSTANDARD2_0_OR_GREATER || NETCOREAPP2_0_OR_GREATER
     /// <inheritdoc cref="char.GetUnicodeCategory(char)"/>
     [Pure]
     public static UnicodeCategory GetUnicodeCategory(this char c) => char.GetUnicodeCategory(c);
+#endif
 }

Compile/Source/References/Emik/Text/Stringifier.cs
@@ -3,12 +3,13 @@
 #if NET35 && WAWA
 namespace Wawa.Modules;
 #else
-// ReSharper disable CheckNamespace
 namespace Emik.Morsels;
-#endif
-
+#if !(NET20 || NET30)
 using static System.Linq.Expressions.Expression;
 using Expression = System.Linq.Expressions.Expression;
+#endif
+
+#endif
 
 /// <summary>Provides stringification methods.</summary>
 // ReSharper disable once BadPreprocessorIndent
@@ -144,6 +145,7 @@ public
         return stringBuilder.ToString();
     }
 
+#if !NET20 && !NET30 && !NETSTANDARD || NETSTANDARD2_0_OR_GREATER
     /// <summary>Gets the type name, with its generics extended.</summary>
     /// <param name="type">The <see cref="Type"/> to get the name of.</param>
     /// <returns>The name of the parameter <paramref name="type"/>.</returns>
@@ -157,6 +159,7 @@ public
         type is null ? Null :
         s_unfoldedNames.TryGetValue(type, out var val) ? val :
         s_unfoldedNames[type] = type.IsGenericType ? $"{type.UnfoldedName(new())}" : type.Name;
+#endif
 
     /// <summary>Converts a number to an ordinal.</summary>
     /// <param name="i">The number to convert.</param>
@@ -254,7 +257,6 @@ public
 
     static void AppendKeyValuePair(this StringBuilder builder, string key, string value) =>
         builder.Append(key).Append(KeyValueSeparator).Append(value);
-#endif
 
     // ReSharper disable once ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
     [MustUseReturnValue]
@@ -262,6 +264,7 @@ public
         p.CanRead &&
         p.GetIndexParameters().Length is 0 &&
         p.GetCustomAttributes(true).All(x => x?.GetType() != typeof(ObsoleteAttribute));
+#endif
 
     [Pure]
     static int Mod(this in int i) => Math.Abs(i) / 10 % 10 == 1 ? 0 : Math.Abs(i) % 10;
@@ -275,8 +278,8 @@ public
             3 => ThirdOrd,
             _ => Else,
         }}";
-#if !NET20 && !NET30 && !NETSTANDARD || NETSTANDARD2_0_OR_GREATER
 
+#if !NET20 && !NET30 && !NETSTANDARD || NETSTANDARD2_0_OR_GREATER
     [Pure]
     static StringBuilder EnumeratorStringifier(this IEnumerator iterator)
     {

Compile/Source/References/System/Diagnostics/Attributes.cs
@@ -342,7 +342,7 @@ namespace System.Runtime.CompilerServices
     /// <summary>
     /// Indicates that a method is an extension method, or that a class or assembly contains extension methods.
     /// </summary>
-    [AttributeUsage(Method | Class | AttributeTargets.Assembly)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
     sealed partial class ExtensionAttribute : Attribute { }
 #endif
 #if !NET6_0_OR_GREATER

commit 53faf1c09f2dbb1c196215b4f0277c9446e354be

Re-support .NET 3.5, which I accidentally broke after a6c6eebea85827ab07fc8e8429b3f7a6ad3f28ed

Compile/Source/References/Emik/Text/EnumStrings.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-#if NET35_OR_GREATER || NETSTANDARD2_0_OR_GREATER || NETCOREAPP2_0_OR_GREATER
+#if NET40_OR_GREATER || NETSTANDARD2_0_OR_GREATER || NETCOREAPP2_0_OR_GREATER
 // ReSharper disable once CheckNamespace RedundantNameQualifier
 namespace Emik.Morsels;
 