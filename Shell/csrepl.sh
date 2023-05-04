72e721c9389f716d5c66e6cd011b44fcfad118c8
Emik <emik.lakunishok@gmail.com>
2023-05-05 01:21:41 +0200

    Use project

--- Shell/csrepl.sh
+++ Shell/csrepl.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-csharprepl -r "nuget: JetBrains.Annotations, 2023.2.0-eap2" "nuget: Emik.Results, 1.0.5" "nuget: Emik.Unions, 1.0.4" "$(dirname "$(dirname "$(realpath "$0")")")/Experimental/REPL.csx"
+morsels="$(dirname "$(dirname "$(realpath "$0")")")"
+above="$(dirname $morsels)"
+
+csharprepl -r "nuget: JetBrains.Annotations, 2023.2.0-eap2" "$above/Emik.Unions/Emik.Unions.csproj" "$morsels/Experimental/REPL.csx"
 
