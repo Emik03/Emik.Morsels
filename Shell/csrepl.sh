775218e692bd7a10b61b543bcee92749dd0d4a84
Emik <emik.lakunishok@gmail.com>
2023-05-05 02:23:32 +0200

    Fix REPL

--- Shell/csrepl.sh
+++ Shell/csrepl.sh
@@ -2,5 +2,5 @@
 morsels="$(dirname "$(dirname "$(realpath "$0")")")"
 above="$(dirname $morsels)"
 
-csharprepl -r "nuget: JetBrains.Annotations, 2023.2.0-eap2" "$above/Emik.Unions/Emik.Unions.csproj" "$morsels/Experimental/REPL.csx"
+csharprepl -r "nuget: JetBrains.Annotations, 2023.2.0-eap2" "nuget: Emik.Results, 1.1.1" "nuget: Emik.Unions, 1.1.2" "$morsels/Experimental/REPL.csx"
 
