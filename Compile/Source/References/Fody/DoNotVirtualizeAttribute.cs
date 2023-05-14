a7d48cc93356494ad170d6c73bd26dccd74e7f79
Emik <emik.lakunishok@gmail.com>
2023-05-14 20:53:53 +0200

    Polyfill DoNotVirtualizeAttribute

---
+++ Compile/Source/References/Fody/DoNotVirtualizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#if NETFRAMEWORK && !NET45_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace Virtuosity;
+
+using static AttributeTargets;
+
+/// <summary>Used to exclude a class form virtualization.</summary>
+[AttributeUsage(Class)]
+sealed partial class DoNotVirtualizeAttribute : Attribute { }
+
+#endif
