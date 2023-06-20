1dffbb05c4e082c6ca3c040accfa481b70e8994f
Emik <emik.lakunishok@gmail.com>
2023-06-20 23:57:37 +0200

    Use expression body

--- Compile/Source/References/Emik/Buffers/Skip.cs
+++ Compile/Source/References/Emik/Buffers/Skip.cs
@@ -10,12 +10,10 @@ static partial class Skip
     /// <typeparam name="T">The type of value to skip initialization.</typeparam>
     /// <param name="ret">The value to skip initialization.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Init<T>(out T ret)
-    {
+    public static void Init<T>(out T ret) =>
 #if (NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) && !NO_SYSTEM_MEMORY
         Unsafe.SkipInit(out ret);
 #else
         ret = default;
 #endif
-    }
 }
