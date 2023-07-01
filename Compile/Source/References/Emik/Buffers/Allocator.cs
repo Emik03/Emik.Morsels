29c4595d69976a5ff3baeff9560dd1b59158e752
Emik <emik.lakunishok@gmail.com>
2023-07-01 14:27:21 +0200

    Remove functions deemed to unsafe

--- Compile/Source/References/Emik/Buffers/Allocator.cs
+++ Compile/Source/References/Emik/Buffers/Allocator.cs
@@ -3,50 +3,9 @@
 // ReSharper disable once CheckNamespace
 namespace Emik.Morsels;
 #pragma warning disable 8500
-using static Span;
-
 /// <summary>Provides the method to convert spans.</summary>
 static partial class Allocator
 {
-#if !CSHARPREPL
-    /// <summary>Allocates the buffer on the stack or heap, and gives it to the caller.</summary>
-    /// <remarks><para>
-    /// This method is aggressively inlined.
-    /// </para><para>
-    /// See <see cref="StackallocSize"/> for details about stack- and heap-allocation.
-    /// </para></remarks>
-    /// <typeparam name="T">The type of buffer.</typeparam>
-    /// <param name="length">The length of the buffer.</param>
-    /// <returns>The allocated buffer.</returns>
-    [Inline, MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe Span<T> Alloc<T>(this int length)
-#if UNMANAGED_SPAN
-        where T : unmanaged
-#endif
-        =>
-            length switch
-            {
-                <= 0 => default, // No allocation needed
-                _ when length <= StackallocSize / sizeof(T) => length.Stackalloc<T>(), // Stack-allocated buffer
-                _ => new T[length], // Heap-allocated buffer
-            };
-
-    /// <summary>Stack-allocates the buffer, and gives it to the caller.</summary>
-    /// <remarks><para>This method is aggressively inlined.</para></remarks>
-    /// <typeparam name="T">The type of buffer.</typeparam>
-    /// <param name="length">The length of the buffer.</param>
-    /// <returns>The stack-allocated buffer.</returns>
-    [Inline, MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe Span<T> Stackalloc<T>(this int length)
-#if UNMANAGED_SPAN
-        where T : unmanaged
-#endif
-    {
-        var pointer = stackalloc byte[unchecked(sizeof(T) * length)];
-        return MemoryMarshal.CreateSpan(ref *(T*)pointer, length);
-    }
-#endif
-
     /// <summary>Reinterprets the span as a series of managed types.</summary>
     /// <typeparam name="T">The type of span to convert to.</typeparam>
     /// <param name="span">The span to convert.</param>
@@ -55,7 +14,7 @@ static partial class Allocator
     /// but with each time assumed to be <typeparamref name="T"/>.
     /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe Span<T> As<T>(this in Span<nint> span)
+    public static unsafe Span<T> Reinterpret<T>(this scoped in Span<nint> span)
         where T : class =>
         MemoryMarshal.CreateSpan(ref *(T*)span[0], span.Length);
 
@@ -67,7 +26,7 @@ static partial class Allocator
     /// but with each time assumed to be <typeparamref name="T"/>.
     /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe Span<T> As<T>(this in Span<nuint> span)
+    public static unsafe Span<T> Reinterpret<T>(this scoped in Span<nuint> span)
         where T : class =>
         MemoryMarshal.CreateSpan(ref *(T*)span[0], span.Length);
 
@@ -79,7 +38,7 @@ static partial class Allocator
     /// but with each time assumed to be <typeparamref name="T"/>.
     /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe ReadOnlySpan<T> As<T>(this in ReadOnlySpan<nint> span) =>
+    public static unsafe ReadOnlySpan<T> Reinterpret<T>(this scoped in ReadOnlySpan<nint> span) =>
         MemoryMarshal.CreateReadOnlySpan(ref *(T*)span[0], span.Length);
 
     /// <summary>Reinterprets the span as a series of managed types.</summary>
@@ -90,7 +49,7 @@ static partial class Allocator
     /// but with each time assumed to be <typeparamref name="T"/>.
     /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe ReadOnlySpan<T> As<T>(this in ReadOnlySpan<nuint> span) =>
+    public static unsafe ReadOnlySpan<T> Reinterpret<T>(this scoped in ReadOnlySpan<nuint> span) =>
         MemoryMarshal.CreateReadOnlySpan(ref *(T*)span[0], span.Length);
 
     /// <inheritdoc cref="Raw{T}(T)" />
