16d5457fb0c518af724d61aff078ec4082ca1881
Emik <emik.lakunishok@gmail.com>
2023-07-29 16:42:58 +0200

    Redirect to make

--- Shell/csr.sh
+++ Shell/csr.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 morsels="$(dirname "$(dirname "$(realpath "$0")")")"
-above="$(dirname $morsels)"
-
-csharprepl -r "nuget: CommunityToolkit.Common, 8.2.1" "nuget: CommunityToolkit.Diagnostics, 8.2.1" "nuget: CommunityToolkit.HighPerformance, 8.2.1" "nuget: TextCopy, 6.2.1" "nuget: JetBrains.Annotations, 2023.2.0" "nuget: Emik.Results, 2.1.1" "nuget: Emik.Unions, 2.1.1" "$morsels/Snippets/REPL.csx"
+make -C "$morsels" repl
 
