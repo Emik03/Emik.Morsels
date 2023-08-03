3aa97e5ab20532a4406e5743934028580c4eefd8
Emik <emik.lakunishok@gmail.com>
2023-08-03 22:36:56 +0200

    Remove unstable APIs

--- Compile/Source/References/Emik/Buffers/Allocator.cs
+++ Compile/Source/References/Emik/Buffers/Allocator.cs
@@ -2,97 +2,12 @@
 
 // ReSharper disable CheckNamespace RedundantUsingDirective
 namespace Emik.Morsels;
-#pragma warning disable 8500
+#pragma warning disable 1574 8500
 using static Span;
 
 /// <summary>Provides the method to convert spans.</summary>
 static partial class Allocator
 {
-#if !CSHARPREPL
-#if (NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) && !NO_SYSTEM_MEMORY
-    /// <summary>Allocates the buffer on the stack or heap, and gives it to the caller.</summary>
-    /// <remarks><para>
-    /// This method is aggressively inlined.
-    /// </para><para>
-    /// See <see cref="StackallocSize"/> for details about stack- and heap-allocation.
-    /// </para></remarks>
-    /// <typeparam name="T">The type of buffer.</typeparam>
-    /// <param name="length">The length of the buffer.</param>
-    /// <returns>The allocated buffer.</returns>
-    [Inline, MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Span<T> Alloc<T>(this in int length)
-#if UNMANAGED_SPAN
-        where T : unmanaged
-#endif
-        =>
-            length <= 0 ? default :
-            IsStack<T>(length) ? length.Stackalloc<T>() : new T[length];
-#endif
-#if NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP
-    /// <summary>Stack-allocates the buffer, and gives it to the caller.</summary>
-    /// <remarks><para>This method is aggressively inlined.</para></remarks>
-    /// <typeparam name="T">The type of buffer.</typeparam>
-    /// <param name="length">The length of the buffer.</param>
-    /// <returns>The stack-allocated buffer.</returns>
-    [Inline, MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe Span<T> Stackalloc<T>(this in int length)
-#if UNMANAGED_SPAN
-        where T : unmanaged
-#endif
-    {
-        var pointer = stackalloc byte[InBytes<T>(length)];
-        return MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(pointer), length);
-    }
-#endif
-#endif
-#if (NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) && !NO_SYSTEM_MEMORY
-    /// <summary>Reinterprets the span as a series of managed types.</summary>
-    /// <typeparam name="T">The type of span to convert to.</typeparam>
-    /// <param name="span">The span to convert.</param>
-    /// <returns>
-    /// The span that points to the same region as <paramref name="span"/>
-    /// but with each time assumed to be <typeparamref name="T"/>.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Span<T> Reinterpret<T>(this Span<nint> span)
-        where T : class =>
-        MemoryMarshal.CreateSpan(ref Unsafe.As<nint, T>(ref span[0]), span.Length);
-
-    /// <summary>Reinterprets the span as a series of managed types.</summary>
-    /// <typeparam name="T">The type of span to convert to.</typeparam>
-    /// <param name="span">The span to convert.</param>
-    /// <returns>
-    /// The span that points to the same region as <paramref name="span"/>
-    /// but with each time assumed to be <typeparamref name="T"/>.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Span<T> Reinterpret<T>(this Span<nuint> span)
-        where T : class =>
-        MemoryMarshal.CreateSpan(ref Unsafe.As<nuint, T>(ref span[0]), span.Length);
-
-    /// <summary>Reinterprets the span as a series of managed types.</summary>
-    /// <typeparam name="T">The type of span to convert to.</typeparam>
-    /// <param name="span">The span to convert.</param>
-    /// <returns>
-    /// The span that points to the same region as <paramref name="span"/>
-    /// but with each time assumed to be <typeparamref name="T"/>.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ReadOnlySpan<T> Reinterpret<T>(this ReadOnlySpan<nint> span) =>
-        MemoryMarshal.CreateSpan(ref Unsafe.As<nint, T>(ref Unsafe.AsRef(in span[0])), span.Length);
-
-    /// <summary>Reinterprets the span as a series of managed types.</summary>
-    /// <typeparam name="T">The type of span to convert to.</typeparam>
-    /// <param name="span">The span to convert.</param>
-    /// <returns>
-    /// The span that points to the same region as <paramref name="span"/>
-    /// but with each time assumed to be <typeparamref name="T"/>.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ReadOnlySpan<T> Reinterpret<T>(this ReadOnlySpan<nuint> span) =>
-        MemoryMarshal.CreateSpan(ref Unsafe.As<nuint, T>(ref Unsafe.AsRef(in span[0])), span.Length);
-#endif
-#pragma warning disable 1574
 #if (NET45_OR_GREATER || NETSTANDARD1_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER) && !NO_SYSTEM_MEMORY
     /// <inheritdoc cref="Raw{T}(T)" />
     [Inline, MethodImpl(MethodImplOptions.AggressiveInlining)]
