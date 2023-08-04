44707a1a56339be6b7606c8c768787909a6a2c30
Emik <emik.lakunishok@gmail.com>
2023-08-04 23:20:52 +0200

    Add FixedGenerator

---
+++ Compile/Source/References/Emik/Roslyn/FixedGenerator.cs
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MPL-2.0
+#if ROSLYN
+// ReSharper disable once CheckNamespace
+namespace Emik.Morsels;
+
+/// <summary>Generates the attribute needed to use this analyzer.</summary>
+/// <param name="fileName">The file name of the source.</param>
+/// <param name="contents">The contents of the source.</param>
+[Generator]
+public abstract class FixedGenerator(
+    [StringSyntax(StringSyntaxAttribute.Uri), UriString] string fileName,
+    [StringSyntax("C#")] string contents
+) : ISourceGenerator
+{
+    /// <inheritdoc />
+    void ISourceGenerator.Execute(GeneratorExecutionContext context) => context.AddSource($"{fileName}.g.cs", contents);
+
+    /// <inheritdoc />
+    void ISourceGenerator.Initialize(GeneratorInitializationContext context) { }
+}
+#endif
