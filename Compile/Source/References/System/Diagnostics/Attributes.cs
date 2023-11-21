a795e6078f42e5284269b9f73c78af18aace14d7
Emik <emik.lakunishok@gmail.com>
2023-11-21 21:23:17 +0100

    Polyfill ExperimentalAttribute

--- Compile/Source/References/System/Diagnostics/Attributes.cs
+++ Compile/Source/References/System/Diagnostics/Attributes.cs
@@ -46,6 +46,58 @@ namespace System.Diagnostics.CodeAnalysis
         public bool ParameterValue { get; }
     }
 
+    /// <summary>Indicates that an API is experimental and it may change in the future.</summary>
+    /// <remarks><para>
+    /// This attribute allows call sites to be flagged with a diagnostic that indicates that an experimental
+    /// feature is used. Authors can use this attribute to ship preview features in their assemblies.
+    /// </para></remarks>
+    [AttributeUsage(
+        AttributeTargets.Assembly |
+        AttributeTargets.Class |
+        AttributeTargets.Constructor |
+        AttributeTargets.Delegate |
+        AttributeTargets.Enum |
+        AttributeTargets.Event |
+        AttributeTargets.Field |
+        AttributeTargets.Interface |
+        AttributeTargets.Method |
+        AttributeTargets.Module |
+        AttributeTargets.Property |
+        AttributeTargets.Struct,
+        Inherited = false
+    )]
+    public sealed class ExperimentalAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExperimentalAttribute"/> class, specifying the
+        /// ID that the compiler will use when reporting a use of the API the attribute applies to.
+        /// </summary>
+        /// <param name="diagnosticId">
+        /// The ID that the compiler will use when reporting a use of the API the attribute applies to.
+        /// </param>
+        public ExperimentalAttribute(string diagnosticId) => DiagnosticId = diagnosticId;
+
+        /// <summary>
+        /// Gets the ID that the compiler will use when reporting a use of the API the attribute applies to.
+        /// </summary>
+        /// <value>The unique diagnostic ID.</value>
+        /// <remarks><para>
+        /// The diagnostic ID is shown in build output for warnings and errors.
+        /// This property represents the unique ID that can be used to suppress the warnings or errors, if needed.
+        /// </para></remarks>
+        public string DiagnosticId { get; }
+
+        /// <summary>
+        /// Gets or sets the URL for corresponding documentation. The API accepts a format string
+        /// instead of an actual URL, creating a generic URL that includes the diagnostic ID.
+        /// </summary>
+        /// <value>The format string that represents a URL to corresponding documentation.</value>
+        /// <remarks><para>
+        /// An example format string is <c>https://contoso.com/obsoletion-warnings/{0}</c>.
+        /// </para></remarks>
+        public string? UrlFormat { get; set; }
+    }
+
     /// <summary>Specifies that an output may be null even if the corresponding type disallows it.</summary>
     [AttributeUsage(
         AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue
