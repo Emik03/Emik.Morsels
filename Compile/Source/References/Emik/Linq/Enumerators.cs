0f214f7b32856105c4fc83f4a99262deaaeef98c
Emik <emik.lakunishok@gmail.com>
2023-12-30 22:08:42 +0100

    Add ToList for unusual interfaces

--- Compile/Source/References/Emik/Linq/Enumerators.cs
+++ Compile/Source/References/Emik/Linq/Enumerators.cs
@@ -7,35 +7,26 @@ namespace Emik.Morsels;
 /// <summary>Provides methods to convert <see cref="IEnumerator{T}"/> to <see cref="IEnumerable{T}"/>.</summary>
 static partial class EnumeratorToEnumerable
 {
-    /// <summary>Wraps the enumerator inside an <see cref="IEnumerable{T}"/>.</summary>
-    /// <param name="enumerator">The enumerator to encapsulate.</param>
-    /// <returns>
-    /// The <see cref="IEnumerator{T}"/> instance that returns the parameter <paramref name="enumerator"/>.
-    /// </returns>
-    [Pure]
-    public static IEnumerator<object?> AsGeneric(this IEnumerator enumerator) => new Enumerator(enumerator);
+    /// <summary>Collects <see cref="IComparer"/> and <see cref="IEqualityComparer"/> instances.</summary>
+    sealed class ComparerCollector : IComparer, IEqualityComparer
+    {
+        public List<object?> List { get; } = [];
 
-    /// <summary>Wraps the enumerator inside an <see cref="IEnumerable{T}"/>.</summary>
-    /// <param name="enumerator">The enumerator to encapsulate.</param>
-    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="enumerator"/>.</returns>
-    [LinqTunnel, Pure]
-    public static IEnumerable<object?> AsEnumerable(this IEnumerator enumerator) =>
-#pragma warning disable CA2000, IDISP004
-        enumerator.AsGeneric().AsEnumerable();
-#pragma warning restore CA2000, IDISP004
+        /// <inheritdoc />
+        bool IEqualityComparer.Equals(object? x, object? y) => Append(x, true);
 
-    /// <summary>Wraps the array inside an <see cref="IEnumerable{T}"/>.</summary>
-    /// <param name="array">The array to encapsulate.</param>
-    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="array"/>.</returns>
-    [LinqTunnel, Pure] // ReSharper disable once NotDisposedResource ObjectProducedWithMustDisposeAnnotatedMethodIsNotDisposed
-    public static IEnumerable<object?> AsGenericEnumerable(this Array array) => array.GetEnumerator().AsEnumerable();
+        /// <inheritdoc />
+        int IComparer.Compare(object? x, object? y) => Append(x, 0);
 
-    /// <summary>Wraps the <see cref="IEnumerator{T}"/> inside an <see cref="IEnumerable{T}"/>.</summary>
-    /// <typeparam name="T">The type of item to enumerate.</typeparam>
-    /// <param name="enumerator">The <see cref="IEnumerator{T}"/> to encapsulate.</param>
-    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="enumerator"/>.</returns>
-    [LinqTunnel, Pure]
-    public static IEnumerable<T> AsEnumerable<T>(this IEnumerator<T> enumerator) => new Enumerable<T>(enumerator);
+        /// <inheritdoc />
+        int IEqualityComparer.GetHashCode(object? obj) => Append(obj, 0);
+
+        T Append<T>(object? obj, T ret)
+        {
+            List.Add(obj);
+            return ret;
+        }
+    }
 
     /// <summary>
     /// Wraps an <see cref="IEnumerator{T}"/> and exposes it from an <see cref="IEnumerable{T}"/> context.
@@ -78,5 +69,57 @@ static partial class EnumeratorToEnumerable
 
         /// <inheritdoc />
         public bool MoveNext() => enumerator.MoveNext();
+    }
+
+    /// <summary>Wraps the enumerator inside an <see cref="IEnumerable{T}"/>.</summary>
+    /// <param name="enumerator">The enumerator to encapsulate.</param>
+    /// <returns>
+    /// The <see cref="IEnumerator{T}"/> instance that returns the parameter <paramref name="enumerator"/>.
+    /// </returns>
+    [Pure]
+    public static IEnumerator<object?> AsGeneric(this IEnumerator enumerator) => new Enumerator(enumerator);
+
+    /// <summary>Wraps the enumerator inside an <see cref="IEnumerable{T}"/>.</summary>
+    /// <param name="enumerator">The enumerator to encapsulate.</param>
+    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="enumerator"/>.</returns>
+    [Pure]
+    public static IEnumerable<object?> AsEnumerable(this IEnumerator enumerator) =>
+#pragma warning disable CA2000, IDISP004
+        enumerator.AsGeneric().AsEnumerable();
+#pragma warning restore CA2000, IDISP004
+
+    /// <summary>Wraps the array inside an <see cref="IEnumerable{T}"/>.</summary>
+    /// <param name="array">The array to encapsulate.</param>
+    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="array"/>.</returns>
+    [Pure] // ReSharper disable once NotDisposedResource ObjectProducedWithMustDisposeAnnotatedMethodIsNotDisposed
+    public static IEnumerable<object?> AsGenericEnumerable(this Array array) => array.GetEnumerator().AsEnumerable();
+
+    /// <summary>Wraps the <see cref="IEnumerator{T}"/> inside an <see cref="IEnumerable{T}"/>.</summary>
+    /// <typeparam name="T">The type of item to enumerate.</typeparam>
+    /// <param name="enumerator">The <see cref="IEnumerator{T}"/> to encapsulate.</param>
+    /// <returns>The <see cref="IEnumerator{T}"/> instance that wraps <paramref name="enumerator"/>.</returns>
+    [Pure]
+    public static IEnumerable<T> AsEnumerable<T>(this IEnumerator<T> enumerator) => new Enumerable<T>(enumerator);
+
+    /// <summary>Converts an <see cref="IStructuralComparable"/> to a <see cref="List{T}"/>.</summary>
+    /// <param name="structure">The <see cref="IStructuralComparable"/> to convert.</param>
+    /// <returns>The <see cref="List{T}"/> that contains elements from <paramref name="structure"/>.</returns>
+    [Pure]
+    public static List<object?> ToList(this IStructuralComparable structure)
+    {
+        ComparerCollector collector = new();
+        _ = structure.CompareTo(structure, collector);
+        return collector.List;
+    }
+
+    /// <summary>Converts an <see cref="IStructuralEquatable"/> to a <see cref="List{T}"/>.</summary>
+    /// <param name="structure">The <see cref="IStructuralEquatable"/> to convert.</param>
+    /// <returns>The <see cref="List{T}"/> that contains elements from <paramref name="structure"/>.</returns>
+    [Pure]
+    public static List<object?> ToList(this IStructuralEquatable structure)
+    {
+        ComparerCollector collector = new();
+        _ = structure.Equals(structure, collector);
+        return collector.List;
     }
 }
