c3f2a85002a192958feb5d2ce98acb20b36fac1d
Emik <emik.lakunishok@gmail.com>
2024-01-31 14:10:00 +0100

    Add IsSingle

--- Compile/Source/References/Emik/Collections/Immutable/Bits.Overlaps.cs
+++ Compile/Source/References/Emik/Collections/Immutable/Bits.Overlaps.cs
@@ -12,21 +12,56 @@ readonly
 #endif
     partial struct Bits<T>
 {
+    /// <summary>Determines whether the item has only a single bit.</summary>
+    /// <param name="item">The element to test.</param>
+    /// <returns>
+    /// The value <see langword="true"/> if the parameter <paramref name="item"/>
+    /// has a single bit set; otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+    public static bool IsSingle(in T item) =>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+    	0 switch
+    	{
+    	    _ when typeof(T) == typeof(byte) => BitOperations.IsPow2((byte)(object)item),
+    	    _ when typeof(T) == typeof(sbyte) => BitOperations.IsPow2(unchecked((byte)(sbyte)(object)item)),
+    	    _ when typeof(T) == typeof(short) => BitOperations.IsPow2(unchecked((ushort)(short)(object)item)),
+    	    _ when typeof(T) == typeof(ushort) => BitOperations.IsPow2((ushort)(object)item),
+    	    _ when typeof(T) == typeof(int) => BitOperations.IsPow2(unchecked((uint)(int)(object)item)),
+    	    _ when typeof(T) == typeof(uint) => BitOperations.IsPow2((uint)(object)item),
+    	    _ when typeof(T) == typeof(long) => BitOperations.IsPow2(unchecked((ulong)(long)(object)item)),
+    	    _ when typeof(T) == typeof(ulong) => BitOperations.IsPow2((ulong)(object)item),
+    	    _ when typeof(T) == typeof(nint) => BitOperations.IsPow2(unchecked((nuint)(nint)(object)item)),
+    	    _ when typeof(T) == typeof(nuint) => BitOperations.IsPow2((nuint)(object)item),
+    	    _ when !typeof(T).IsEnum => (Enumerator)item is var e && e.MoveNext() && !e.MoveNext(),
+            _ => (typeof(T) == typeof(Enum) ? item.GetType() : typeof(T)).GetEnumUnderlyingType() switch
+            {
+    	        var x when x == typeof(byte) => BitOperations.IsPow2((byte)(object)item),
+    	        var x when x == typeof(sbyte) => BitOperations.IsPow2(unchecked((byte)(sbyte)(object)item)),
+    	        var x when x == typeof(short) => BitOperations.IsPow2(unchecked((ushort)(short)(object)item)),
+    	        var x when x == typeof(ushort) => BitOperations.IsPow2((ushort)(object)item),
+    	        var x when x == typeof(int) => BitOperations.IsPow2(unchecked((uint)(int)(object)item)),
+    	        var x when x == typeof(uint) => BitOperations.IsPow2((uint)(object)item),
+    	        var x when x == typeof(long) => BitOperations.IsPow2(unchecked((ulong)(long)(object)item)),
+    	        var x when x == typeof(ulong) => BitOperations.IsPow2((ulong)(object)item),
+    	        var x when x == typeof(nint) => BitOperations.IsPow2(unchecked((nuint)(nint)(object)item)),
+    	        var x when x == typeof(nuint) => BitOperations.IsPow2((nuint)(object)item),
+    	        _ => (Enumerator)item is var e && e.MoveNext() && !e.MoveNext(),
+            },
+    	};
+#else
+        (Enumerator)item is var e && e.MoveNext() && !e.MoveNext();
+#endif
+
     /// <inheritdoc cref="ICollection{T}.Contains"/>
     [CollectionAccess(CollectionAccessType.Read), MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     public bool Contains(T item)
     {
-        using var that = GetEnumerator();
+    	if (!IsSingle(item))
+            return false;
 
-        while (that.MoveNext())
-        {
-            var current = that.Current;
-
-            if (Eq(current, item))
-                return true;
-        }
-
-        return false;
+        And(_value, ref item);
+        return !EqZero(item);
     }
 
     /// <inheritdoc cref="ISet{T}.IsProperSubsetOf" />
@@ -107,10 +142,10 @@ readonly
         T t = default;
 
         foreach (var next in other)
-            if ((Enumerator)next is var e && !e.MoveNext() || e.MoveNext())
-                return false;
-            else
+            if (IsSingle(next))
                 Or(next, ref t);
+            else
+                return false;
 
         return Eq(_value, t);
     }
