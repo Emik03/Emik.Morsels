ae2583b237aa08b52524a4a0c33f56342b87afc1
Emik <emik.lakunishok@gmail.com>
2024-05-15 01:02:43 +0200

    Add overload for generic random generation

--- Compile/Source/References/Emik/Numerics/RandomGeneration.cs
+++ Compile/Source/References/Emik/Numerics/RandomGeneration.cs
@@ -3,6 +3,8 @@
 // ReSharper disable once CheckNamespace
 namespace Emik.Morsels;
 
+using static Span;
+
 /// <summary>Extension methods to generate random numbers.</summary>
 static partial class RandomGeneration
 {
@@ -10,23 +12,37 @@ static partial class RandomGeneration
     /// <typeparam name="T">The type of the random value.</typeparam>
     /// <param name="random">The random number generator.</param>
     /// <returns>The random value.</returns>
-    public static unsafe T Next<T>(this Random random)
+    public static unsafe T Next<T>(this Func<int, int, int> random)
         where T : unmanaged
     {
         T output = default;
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
-        random.NextBytes(new Span<byte>(&output, sizeof(T)));
-#else
+
         if (sizeof(T) >= sizeof(int))
             for (var i = 0; i < sizeof(T) / sizeof(int); i++)
-                *((int*)&t + i) = random.Next(int.MinValue, int.MaxValue);
+                *((int*)&output + i) = random(int.MinValue, int.MaxValue);
 
         if (sizeof(T) % sizeof(int) is 2 or 3)
-            *((ushort*)((byte*)&t + sizeof(T)) - 1) = (ushort)random.Next(ushort.MinValue, ushort.MinValue);
+            *((ushort*)((byte*)&output + sizeof(T)) - 1) = (ushort)random(ushort.MinValue, ushort.MinValue);
 
         if (sizeof(T) % sizeof(int) is 1 or 3)
-            *((byte*)&t + sizeof(T) - sizeof(T) % 4) = (byte)random.Next(byte.MinValue, byte.MinValue);
-#endif
+            *((byte*)&output + sizeof(T) - sizeof(T) % 4) = (byte)random(byte.MinValue, byte.MinValue);
+
         return output;
+    }
+
+    /// <summary>Generates a random value of type <typeparamref name="T"/>.</summary>
+    /// <typeparam name="T">The type of the random value.</typeparam>
+    /// <param name="random">The random number generator.</param>
+    /// <returns>The random value.</returns>
+    public static T Next<T>(this Random random)
+        where T : unmanaged
+    {
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+        T output = default;
+        random.NextBytes(To<byte>.From(Ref(ref output)));
+        return output;
+#else
+        return Next<T>(random.Next);
+#endif
     }
 }
