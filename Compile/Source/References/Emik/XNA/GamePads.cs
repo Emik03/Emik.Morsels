3bdcf3980b96e9434cffd0a31a3ced08956c16bf
Emik <emik.lakunishok@gmail.com>
2024-07-07 00:15:55 +0200

    Fix default constructor behavior

--- Compile/Source/References/Emik/XNA/GamePads.cs
+++ Compile/Source/References/Emik/XNA/GamePads.cs
@@ -5,9 +5,9 @@ namespace Emik.Morsels;
 
 /// <summary>Provides the enumeration over <see cref="GamePad"/> instances.</summary>
 [StructLayout(LayoutKind.Auto)]
-struct GamePads(PlayerIndex? last = PlayerIndex.Four) : IEnumerable<GamePadState>, IEnumerator<GamePadState>
+struct GamePads(PlayerIndex last = PlayerIndex.Four) : IEnumerable<GamePadState>, IEnumerator<GamePadState>
 {
-    readonly PlayerIndex _last = last ?? PlayerIndex.Four;
+    readonly PlayerIndex _length = last + 1;
 
     PlayerIndex _index;
 
@@ -24,8 +24,10 @@ struct GamePads(PlayerIndex? last = PlayerIndex.Four) : IEnumerable<GamePadState
     public void Reset() => _index = PlayerIndex.One;
 
     /// <inheritdoc />
-    public bool MoveNext() => _index <= _last && (Current = GamePad.GetState(_index++)) is var _;
-
+    public bool MoveNext() =>
+#pragma warning disable MA0099
+        _index < (_length is 0 ? PlayerIndex.Four + 1 : _length) && (Current = GamePad.GetState(_index++)) is var _;
+#pragma warning restore MA0099
     /// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
     public readonly GamePads GetEnumerator() => this;
 
