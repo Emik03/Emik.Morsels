24d6252e518bb50871c85d689099a04ad8d6c7a1
Emik <emik.lakunishok@gmail.com>
2024-08-11 22:54:36 +0200

    Reorder conditions

--- Compile/Source/References/AssemblyLevelAttributes.cs
+++ Compile/Source/References/AssemblyLevelAttributes.cs
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
-#if WAWA
-[assembly: CLSCompliant(true), NullGuard(ValidationFlags.AllPublicArguments)]
-#else
 [assembly: CLSCompliant(true)]
-#endif
 #if ROSLYN
 [assembly: NullGuard(ValidationFlags.None)]
+#elif WAWA
+[assembly: NullGuard(ValidationFlags.AllPublicArguments)]
 #endif
