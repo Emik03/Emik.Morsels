e82dc45c147bab0c6721bc9a52863c9ef801cf94
Emik <emik.lakunishok@gmail.com>
2024-09-26 09:04:15 +0200

    Encourage inlining identity functions

--- Compile/Source/References/Emik/Collections/NullableItems.cs
+++ Compile/Source/References/Emik/Collections/NullableItems.cs
@@ -11,7 +11,7 @@ static partial class NullableItems
     /// <typeparam name="T">The type of item to adjust nullability.</typeparam>
     /// <param name="iterable">The item to return with adjusted nullability.</param>
     /// <returns>The parameter <paramref name="iterable"/>, with <c>ItemCanBeNullAttribute</c>.</returns>
-    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     [return: NotNullIfNotNull(nameof(iterable))]
     public static IEnumerable<T?>? ItemCanBeNull<T>(this IEnumerable<T>? iterable) => iterable;
 
@@ -19,7 +19,7 @@ static partial class NullableItems
     /// <typeparam name="T">The type of item to adjust nullability.</typeparam>
     /// <param name="iterator">The item to return with adjusted nullability.</param>
     /// <returns>The parameter <paramref name="iterator"/>, with <c>ItemCanBeNullAttribute</c>.</returns>
-    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     [return: NotNullIfNotNull(nameof(iterator))]
     public static IEnumerator<T?>? ItemCanBeNull<T>(this IEnumerator<T>? iterator) => iterator;
 
@@ -27,7 +27,7 @@ static partial class NullableItems
     /// <typeparam name="T">The type of item to adjust nullability.</typeparam>
     /// <param name="collection">The item to return with adjusted nullability.</param>
     /// <returns>The parameter <paramref name="collection"/>, with <c>ItemCanBeNullAttribute</c>.</returns>
-    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     [return: NotNullIfNotNull(nameof(collection))]
     public static IReadOnlyCollection<T?>? ItemCanBeNull<T>(this IReadOnlyCollection<T>? collection) => collection;
 
@@ -35,7 +35,7 @@ static partial class NullableItems
     /// <typeparam name="T">The type of item to adjust nullability.</typeparam>
     /// <param name="list">The item to return with adjusted nullability.</param>
     /// <returns>The parameter <paramref name="list"/>, with <c>ItemCanBeNullAttribute</c>.</returns>
-    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     [return: NotNullIfNotNull(nameof(list))]
     public static IReadOnlyList<T?>? ItemCanBeNull<T>(this IReadOnlyList<T>? list) => list;
 
@@ -43,7 +43,7 @@ static partial class NullableItems
     /// <typeparam name="T">The type of item to adjust nullability.</typeparam>
     /// <param name="set">The item to return with adjusted nullability.</param>
     /// <returns>The parameter <paramref name="set"/>, with <c>ItemCanBeNullAttribute</c>.</returns>
-    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
     [return: NotNullIfNotNull(nameof(set))]
     public static IReadOnlySet<T?>? ItemCanBeNull<T>(this IReadOnlySet<T>? set) => set;
 
