d9754056badca6a0ec746c880eee5fd9733af559
Emik <emik.lakunishok@gmail.com>
2024-10-09 10:09:05 +0200

    Move attribute

--- Compile/Source/References/AssemblyLevelAttributes.cs
+++ Compile/Source/References/AssemblyLevelAttributes.cs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 [assembly: CLSCompliant(true)]
-#if ROSLYN
+#if NET452
+[assembly: RemoveReference("System.Runtime, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
+#elif ROSLYN
 [assembly: NullGuard(ValidationFlags.None)]
 #elif WAWA
 [assembly: NullGuard(ValidationFlags.AllPublicArguments)]
--- Compile/Source/References/Fody/RemoveReferenceAttribute.cs
+++ Compile/Source/References/Fody/RemoveReferenceAttribute.cs
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
 #if NET452
-[assembly: RemoveReference("System.Runtime, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
-
 // ReSharper disable once CheckNamespace
 namespace RemoveReference;
 
