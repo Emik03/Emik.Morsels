c774c9042991eb2bb0a1ea540a3a9d1a9ba198c2
Emik <emik.lakunishok@gmail.com>
2024-10-17 19:32:51 +0200

    Add width and height functions

--- Compile/Source/References/Emik/XNA/ResolutionScaling.cs
+++ Compile/Source/References/Emik/XNA/ResolutionScaling.cs
@@ -6,6 +6,20 @@ namespace Emik.Morsels;
 /// <summary>Contains scaling methods for resolutions.</summary>
 static partial class ResolutionScaling
 {
+    /// <summary>Gets the height of the screen.</summary>
+    /// <param name="device">The current width.</param>
+    /// <returns>The width of the screen.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+    public static int Height(this GraphicsDevice device) =>
+        OperatingSystem.IsAndroid() ? device.DisplayMode.Height : device.Viewport.Height;
+
+    /// <summary>Gets the width of the screen.</summary>
+    /// <param name="device">The current width.</param>
+    /// <returns>The width of the screen.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+    public static int Width(this GraphicsDevice device) =>
+        OperatingSystem.IsAndroid() ? device.DisplayMode.Width : device.Viewport.Width;
+
     /// <summary>Gets the resolution for <see cref="SpriteBatch.Draw(Texture2D, Rectangle, Color)"/>.</summary>
     /// <param name="device">The current width and height.</param>
     /// <param name="w">The native width.</param>
@@ -13,10 +27,7 @@ static partial class ResolutionScaling
     /// <returns>The <see cref="Rectangle"/> for <see cref="SpriteBatch.Draw(Texture2D, Rectangle, Color)"/>.</returns>
     public static Rectangle Resolution(this GraphicsDevice device, float w, float h)
     {
-        var (currentWidth, currentHeight) = OperatingSystem.IsAndroid()
-            ? (device.DisplayMode.Width, device.DisplayMode.Height)
-            : (device.Viewport.Width, device.Viewport.Height);
-
+        var (currentWidth, currentHeight) = (device.Width(), device.Height());
         var (scaledWidth, scaledHeight) = (currentWidth / w, currentHeight / h);
         var min = scaledWidth.Min(scaledHeight);
         var (width, height) = ((int)(min * w), (int)(min * h));
