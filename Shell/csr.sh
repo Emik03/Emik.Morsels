337cbfee026093207e5d2302ed1cea9601804ced
Emik <emik.lakunishok@gmail.com>
2025-02-07 15:12:37 +0100

    Use latest CommunityToolkit

--- Shell/csr.sh
+++ Shell/csr.sh
@@ -3,4 +3,4 @@ morsels="$(dirname "$(dirname "$(realpath "$0")")")"
 make -C "$morsels" Snippets/REPL.csx
 command -v csharprepl || dotnet tool install -g csharprepl
 
-exec csharprepl -r "nuget: CommunityToolkit.Common, 8.4.0-preview3" "nuget: CommunityToolkit.Diagnostics, 8.4.0-preview3" "nuget: CommunityToolkit.HighPerformance, 8.4.0-preview3" "nuget: FastGenericNew, 3.3.1" "nuget: JetBrains.Annotations, 2024.3" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
+exec csharprepl -r "nuget: CommunityToolkit.Common, 8.4.0" "nuget: CommunityToolkit.Diagnostics, 8.4.0" "nuget: CommunityToolkit.HighPerformance, 8.4.0" "nuget: FastGenericNew, 3.3.1" "nuget: JetBrains.Annotations, 2024.3" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
