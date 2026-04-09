26a56099620744debab75121034627b229efb1bd
Emik <emik.cuber@gmail.com>
2026-04-09 14:14:48 +0200

    Prune references

--- Shell/csr.sh
+++ Shell/csr.sh
@@ -3,4 +3,4 @@ morsels="$(dirname "$(dirname "$(realpath "$0")")")"
 make -C "$morsels" Snippets/REPL.csx
 command -v csharprepl || dotnet tool install -g csharprepl
 
-exec csharprepl -r "nuget: CommunityToolkit.Common, 8.4.2" "nuget: CommunityToolkit.Diagnostics, 8.4.2" "nuget: CommunityToolkit.HighPerformance, 8.4.2" "nuget: JetBrains.Annotations, 2025.2.4" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
+exec csharprepl -r "nuget: CommunityToolkit.HighPerformance, 8.4.2" "nuget: JetBrains.Annotations, 2025.2.4" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
