d84e4e512239179a55698cdc89f965580f915979
Emik <emik.cuber@gmail.com>
2026-06-30 11:30:32 +0200

    Bump package version

--- Shell/csr.sh
+++ Shell/csr.sh
@@ -3,4 +3,4 @@ morsels="$(dirname "$(dirname "$(realpath "$0")")")"
 make -C "$morsels" Snippets/REPL.csx
 command -v csharprepl || dotnet tool install -g csharprepl
 
-exec csharprepl -r "nuget: CommunityToolkit.HighPerformance, 8.4.2" "nuget: JetBrains.Annotations, 2025.2.4" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
+exec csharprepl -r "nuget: CommunityToolkit.HighPerformance, 8.4.2" "nuget: JetBrains.Annotations, 2026.2.0" "nuget: TextCopy, 6.2.1" "$morsels/Snippets/REPL.csx"
